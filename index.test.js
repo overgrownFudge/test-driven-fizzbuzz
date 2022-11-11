fd69ca59d06bf7917abfafa36c6d4bdaedede74c
overgrownFudge <overgrownfudge@gmail.com>
2022-11-11 10:30:35 -0500

    added all possible test cases

--- index.test.js
+++ index.test.js
@@ -6,6 +6,16 @@ describe('fizzBuzz()', () => {
     expect(fizzBuzz(30)).toBe(`FizzBuzz`);
   });
 
+  it('returns "Buzz" for multiples of 3', () => {
+    expect(fizzBuzz(6)).toBe("Buzz")
+    expect(fizzBuzz(12)).toBe("Buzz")
+  })
+
+  it('returns "Fizz" for multiples of 5', () => {
+    expect(fizzBuzz(10)).toBe('Fizz')
+    expect(fizzBuzz(25)).toBe('Fizz')
+  })
+
   it('reurns the given number of multiples of neither 3 nor 5', () => {
     expect(fizzBuzz(1)).toBe('1')
     expect(fizzBuzz(22)).toBe('22')

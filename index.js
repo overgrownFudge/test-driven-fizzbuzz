735b61c2344e1ebb494f1a141586bc1038549937
overgrownFudge <overgrownfudge@gmail.com>
2022-11-11 10:30:06 -0500

    function for "Fizz" & "Buzz"

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 module.exports = (num) => {
-  if (num % 15 === 0) return 'FizzBuzz';
+  if (num % 15 === 0) {
+    return 'FizzBuzz';
+  } else if (num % 3 === 0) {
+    return 'Buzz'
+  } else if (num % 5 === 0) {
+    return 'Fizz';
+  }
+
   return `${num}`
 };
